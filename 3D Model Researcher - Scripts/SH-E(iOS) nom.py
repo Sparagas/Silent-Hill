a23e8efd596210846b2a5ee584e83180d9a1a164
Laurynas Zubavicius <55561926+Sparagas@users.noreply.github.com>
2024-01-09 21:41:12 +0200

    Update and Error Fix
    Update Silent Hill: The Escape files
    Update Notepad++ language files
    Fix Silent Hill 4 (PC) texture binary template

--- 3D Model Researcher - Scripts/SH-E(iOS) nom.py
+++ 3D Model Researcher - Scripts/SH-E(iOS) nom.py
@@ -4,53 +4,40 @@ import mrp
 f = mrp.get_bfile()
 
 magic = f.read(4)
-mesh_count = f.readInt()
-for m in range(mesh_count):
+num_mesh = f.readInt()
+for mesh in range(num_mesh):
     mesh_name = f.read(16).decode('utf-8').strip('\x00')
     
     mesh = mrp.create_mesh(mesh_name)
     mat = mrp.create_material(mesh_name)
     mesh.set_material(mat)
 
-    index_count = f.readInt()
-    index = []                      #
-    for j in range(index_count):    #
-        index.append(f.readShort()) #
+    num_index = f.readInt()
+    index = [f.readShort() for _ in range(num_index)]
     # Index
     mesh.set_faces(index)
-    position_count = f.readInt()
-    position = []                       #
-    for j in range(position_count):     #
-        position.append(f.read3Float()) #
+    num_position = f.readInt()
+    position = [f.read3Float() for _ in range(num_position)]
     # Position
     mesh.set_vertices(position)
-
     # Color
-    unkn_0 = f.read(4)
-    color_index_count = f.readInt()
-    if color_index_count != 0:
-        color_index = []                      #
-        for j in range(color_index_count):    #
-            color_index.append(f.readShort()) #
-        color_count = f.readInt()
-        color = []                   #
-        for j in range(color_count): #
-            color.append(f.read(16)) #
+    unkn0 = f.read(4)
+    num_color_index = f.readInt()
+    if num_color_index != 0:
+        color_index = [f.readShort() for _ in range(num_color_index)]
+        num_color = f.readInt()
+        color = [f.read(16) for _ in range(num_color)]
     unkn1 = f.read(154)
     texture_name = f.read(20).decode('utf-8').strip('\x00')
     if texture_name != '':
 
-        mat.set_texture(texture_name[:-3] + 'not.tga', 'RGBA')    # <-
+        # mat.set_texture(texture_name[:-3] + 'not.tga', 'RGBA')    # <-
 
-        texcoord_index = []                      #
-        for j in range (index_count):            #
-            texcoord_index.append(f.readShort()) #
+        texcoord_index = [f.readShort() for _ in range(num_index)]
         # Texcoord Index
         mesh.set_uvs_indices(texcoord_index)
-        texcoord_count = f.readInt()
-        texcoord = []                         #
-        for j in range(texcoord_count):       #
-            texcoord.append(f.read2Float())   #
+        num_texcoord = f.readInt()
+        texcoord = [f.read2Float() for _ in range(num_texcoord)]
         # Texcoord
         mesh.set_uvs(texcoord)
 
