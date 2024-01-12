02c9a906c2d6988b32f312accd523349a1404db3
Laurynas Zubavicius <55561926+Sparagas@users.noreply.github.com>
2024-01-13 00:23:00 +0200

    Silent Hill: The Escape .pvr image
    add Noesis Python plugin for Silent Hill: The Escape .pvr image
    add 010 Editor - Binary Template for Silent Hill: The Escape .pvr image
    update .not with credits

---
+++ Noesis - Python Plugins/fmt_she_ios_pvr.py
@@ -0,0 +1,34 @@
+# Author:
+# Laurynas Zubavičius (Sparagas)
+
+from inc_noesis import *
+
+
+def registerNoesisTypes():
+    handle = noesis.register("Silent Hill: The Escape (iOS)", ".pvr")
+    noesis.setHandlerTypeCheck(handle, check_type)
+    noesis.setHandlerLoadRGBA(handle, load_rgba)
+    return 1
+
+
+def check_type(data):
+    if len(data) < 4:
+        return 0
+    if data[:4] != b'PVR4':
+        return 0
+    return 1
+
+
+def load_rgba(data, tex_list):
+    ctx = rapi.rpgCreateContext()
+    bs = NoeBitStream(data)
+    bs.seek(5)
+    dim_pwr = bs.readUByte()
+    dim = 2 ** dim_pwr
+    img_buf = data[8:]
+    img_buf = rapi.imageDecodePVRTC(img_buf, dim, dim, 4)
+    img_buf = rapi.imageFlipRGBA32(img_buf, dim, dim, 0, 1)
+    img_buf = NoeTexture(rapi.getInputName(), dim, dim, img_buf, noesis.NOESISTEX_RGBA32)
+    img_buf.setFlags(noesis.NTEXFLAG_FILTER_NEAREST)
+    tex_list.append(img_buf)
+    return 1
