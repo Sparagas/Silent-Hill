24e61f345f927b482a9507329e0956e6f219b418
Laurynas Zubavicius <55561926+Sparagas@users.noreply.github.com>
2024-01-29 17:24:31 +0200

    Update inc_sh4_pc_textures.py
    Now displays all images with all mipmaps

--- Noesis - Python Plugins/inc_sh4_pc_textures.py
+++ Noesis - Python Plugins/inc_sh4_pc_textures.py
@@ -64,34 +64,38 @@ def sh4_pc_textures(data, tex_list):
             bs.seek(4, NOESEEK_REL)
             self.ofs_mipmap_dat = [bs.readUInt() for _ in range(7)]
             bs.seek(4, NOESEEK_REL)
-    img_hdr = [ImgHdr()] * num_img
+    img_hdr = [ImgHdr] * num_img
     for i in range(num_img):
         bs.seek(ofs_pal_hdr[i] + ps2_pal[i].ofs)
-        # img_hdr[i] = [ImgHdr()] * ps2_pal[i].num_pal
-        # for j in range(ps2_pal[i].num_pal):
-            # img_hdr[i][j] = ImgHdr()
-            # print(img_hdr[i][j].w, img_hdr[i][j].h)
-        img_hdr[i] = ImgHdr()
-        print(img_hdr[i].w, img_hdr[i].h)
-
+        img_hdr[i] = [ImgHdr] * ps2_pal[i].num_pal
+        for j in range(ps2_pal[i].num_pal):
+            img_hdr[i][j] = ImgHdr()
 
     img_buf = [0] * num_img
     for i in range(num_img):
-        img_buf[i] = [0] * img_hdr[i].num_mipmap
-        for j in range(img_hdr[i].num_mipmap):
-            bs.seek(ofs_pal_hdr[i] + ps2_pal[i].ofs + img_hdr[i].ofs_mipmap_dat[j])
-            img_buf[i][j] = bs.readBytes(img_hdr[i].len_mipmap[j])
-            
-            if img_hdr[i].img_fmt == 'RGBA8888':
-                img = rapi.imageDecodeRaw(img_buf[i][j], img_hdr[i].w // (2 ** j), img_hdr[i].h // (2 ** j), 'b8g8r8a8')
-            elif img_hdr[i].img_fmt == 'DXT1':
-                img = rapi.imageDecodeDXT(img_buf[i][j], img_hdr[i].w // (2 ** j), img_hdr[i].h // (2 ** j), noesis.NOESISTEX_DXT1)
-            elif img_hdr[i].img_fmt == 'DXT3':
-                img = rapi.imageDecodeDXT(img_buf[i][j], img_hdr[i].w // (2 ** j), img_hdr[i].h // (2 ** j), noesis.NOESISTEX_DXT3)
-            elif img_hdr[i].img_fmt == 'DXT5':
-                img = rapi.imageDecodeDXT(img_buf[i][j], img_hdr[i].w // (2 ** j), img_hdr[i].h // (2 ** j), noesis.NOESISTEX_DXT5)
+        img_buf[i] = [0] * ps2_pal[i].num_pal
+        for j in range(ps2_pal[i].num_pal):
+            img_buf[i][j] = [0] * img_hdr[i][j].num_mipmap
+            for k in range(img_hdr[i][j].num_mipmap):
+                bs.seek(ofs_pal_hdr[i] + ps2_pal[i].ofs + img_hdr[i][j].ofs_mipmap_dat[k] + j * 112) # hack
+                img_buf[i][j][k] = bs.readBytes(img_hdr[i][j].len_mipmap[k])
+                
+                w = img_hdr[i][j].w // (2 ** k)
+                if w < 4:
+                    w = 4
+                h = img_hdr[i][j].h // (2 ** k)
+                if h < 4:
+                    h = 4
+                
+                if img_hdr[i][j].img_fmt == 'RGBA8888':
+                    img = rapi.imageDecodeRaw(img_buf[i][j][k], w, h, 'b8g8r8a8')
+                elif img_hdr[i][j].img_fmt == 'DXT1':
+                    img = rapi.imageDecodeDXT(img_buf[i][j][k], w, h, noesis.NOESISTEX_DXT1)
+                elif img_hdr[i][j].img_fmt == 'DXT3':
+                    img = rapi.imageDecodeDXT(img_buf[i][j][k], w, h, noesis.NOESISTEX_DXT3)
+                elif img_hdr[i][j].img_fmt == 'DXT5':
+                    img = rapi.imageDecodeDXT(img_buf[i][j][k], w, h, noesis.NOESISTEX_DXT5)
 
-            img = NoeTexture(rapi.getInputName(), img_hdr[i].w // (2 ** j), img_hdr[i].h // (2 ** j), img, noesis.NOESISTEX_RGBA32)
-            img.setFlags(noesis.NTEXFLAG_FILTER_NEAREST)
-            tex_list.append(img)
-
+                img = NoeTexture(rapi.getInputName(), w, h, img, noesis.NOESISTEX_RGBA32)
+                img.setFlags(noesis.NTEXFLAG_FILTER_NEAREST)
+                tex_list.append(img)
